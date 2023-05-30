67f3a253150e0e7639f580115d4e18b3e99f4e2a
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-30 11:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
     //Write your code here
-
+let strongs=document.get ElementsByTagName("strong");
+forEach(strong=>strong.style.color="green");
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let strongs=document.get ElementsByTagName("strong");
+forEach(strong=>strong.style.color="black");
     
 }
